f281d72eb461346169a63c316c349d41afc54d77
Rakesh Bhugra <81642485+rakeshbhugra@users.noreply.github.com>
2025-10-20 02:03:38 +0530

    completed step 1

---
+++ step01_hello_websocket/server.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(data)
+            await websocket.send_text(f"Message text was: {data}")
+    except WebSocketDisconnect:
+        print("Client disconnected")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
